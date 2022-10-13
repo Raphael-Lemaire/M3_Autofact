94a3b071f55876c8272a21ae7d523a2e78c032ce
Raphaël Lemaire <65557665+Raphael-Lemaire@users.noreply.github.com>
2022-10-13 17:44:28 +0200

    database connection

---
+++ AutoFact_projet/AutoFact/AutoFact/class/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace AutoFact
+{
+    class Database
+    {
+        private static Database _instance = null;
+        //private static readonly object _syncObject = new object();
+        private static string conString = @"Data Source= autofact.db";
+        private Database()
+        {
+            SQLiteConnection Conn = new SQLiteConnection(conString);
+            SQLiteCommand command = new SQLiteCommand(Conn);
+            Conn.Open();
+            if (Conn.State == ConnectionState.Open)
+            {
+                Console.WriteLine("successfully connect");
+
+            }
+            else
+            {
+                Console.WriteLine("denied");
+            }
+        }
+        public static Database getInstance()
+        {
+            if (_instance == null)
+            {
+
+                _instance = new Database();
+            }
+            // SQLiteConnection Conn = new SQLiteConnection(conString);
+            //SQLiteCommand command = new SQLiteCommand();
+            return _instance;
+        }
+        public SQLiteConnection getConnection()
+        {
+                SQLiteConnection Conn = new SQLiteConnection(conString);
+                Conn.Open();
+                Console.WriteLine("Connected");
+                return Conn;
+        }
+        //public static SQLiteCommand getCommand()
+        //{
+           // SQL
+        //}
+    }
+}
+
+
+
